1cc1265478cb653950da7b7865e9650a2f53f162
charlyy <cyoon90@yahoo.com>
2014-07-09 10:25:19 -0700

    changed Hello name! into link

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,7 +1,8 @@
 class Resource < ActiveRecord::Base
   belongs_to :user
 
-
-
-  
+	validates :title, presence: true
+	validates :language, presence: true
+	validates :link, presence: true
+	  
 end
