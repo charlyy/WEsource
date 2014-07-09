1ba849d68248f25d22ad5492e49872e43a6431fa
charlyy <cyoon90@yahoo.com>
2014-07-08 20:59:22 -0700

    removed devise gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require angular
+//= require angular-resource
 //= require foundation
-//= require turbolinks
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+def current_user
+    	if session[:remember_token]
+    		@current_user ||= User.find(session[:remember_token])
+        puts @current_user
+        return @current_user
+    	else
+
+    		false
+    	end
+  	end
+
+  	def authenticate_user
+	    if !self.current_user
+	      redirect_to new_session_path
+	    end
+  	end	
 end
